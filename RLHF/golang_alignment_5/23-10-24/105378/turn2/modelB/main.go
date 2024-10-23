1eb329b712ddd2befb308ffa97c6a922fd6fa60e
pankaj-solanki1 <katyarepankaj31@gmail.com>
2024-10-23 18:12:25 +0530

    105378 turn2

--- RLHF/golang_alignment_5/23-10-24/105378/turn2/modelB/main.go
+++ RLHF/golang_alignment_5/23-10-24/105378/turn2/modelB/main.go
@@ -1,58 +1,74 @@
-package main
+package mlpdsl
 
 import (
-	"fmt"
-	"net"
-	"os"
-	"syscall"
+	"math/rand"
+	"testing"
 )
 
-func main() {
-	// Replace this with the actual file path
-	filePath := "/path/to/the/large/file"
+// MLP stands for Multi-Layer Perceptron
+type MLP struct {
+	layers         []int         // Number of neurons in each layer
+	weights        [][][]float64 // Weights between neurons in each layer
+	biases         [][]float64   // Biases for neurons in each layer
+	activationFunc ActivationFunc
+}
 
-	// Create a TCP connection to the FileReceiver microservice
-	conn, err := net.Dial("tcp", "fileReceiver:12345")
-	if err != nil {
-		fmt.Println("Error connecting to FileReceiver:", err)
-		return
+// ActivationFunc defines the activation function used for neurons
+type ActivationFunc func(float64) float64
+
+func TestNewMLP(t *testing.T) {
+	// Assert MLP structure is created correctly
+	mlp := NewMLP(2, 3, 1, ReLU)
+	if len(mlp.layers) != 3 {
+		t.Errorf("Expected 3 layers, got %d", len(mlp.layers))
 	}
-	defer conn.Close()
+	// Add more assertions for weights, biases, etc.
+}
 
-	// Open the file for reading
-	file, err := os.Open(filePath)
-	if err != nil {
-		fmt.Println("Error opening the file:", err)
-		return
+func BenchmarkFeedForward(b *testing.B) {
+	// Measure the time taken to perform feed-forward for a single input
+	mlp := NewMLP(100, 50, 10, ReLU)
+	input := make([]float64, 100)
+	for i := range input {
+		input[i] = rand.Float64()
 	}
-	defer file.Close()
-
-	// Perform zero-copy I/O
-	info, err := file.Stat()
-	if err != nil {
-		fmt.Println("Error stat file:", err)
-		return
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		mlp.FeedForward(input)
 	}
+}
 
-	fmt.Printf("Transferring file %s of size %d bytes\n", filePath, info.Size())
-
-	// Get the file descriptor of the TCP connection
-	connFile, err := conn.(*net.TCPConn).File()
-	if err != nil {
-		fmt.Println("Error getting connection file descriptor:", err)
-		return
+// Stochastic Gradient Descent (SGD) implementation
+func (mlp *MLP) SGD(trainingData [][]float64, targets []float64, learningRate float64, epochs int, batchSize int) {
+	for epoch := 0; epoch < epochs; epoch++ {
+		for i := 0; i < len(trainingData); i += batchSize {
+			batchInputs := trainingData[i:min(i+batchSize, len(trainingData))]
+			batchTargets := targets[i:min(i+batchSize, len(targets))]
+			mlp.trainBatch(batchInputs, batchTargets, learningRate)
+		}
 	}
-	defer connFile.Close() // Don't forget to close this file descriptor
+}
 
-	// Cast file size to int (assuming file size is within int range)
-	fileSize := int(info.Size())
+func (mlp *MLP) trainBatch(inputs [][]float64, targets []float64, learningRate float64) {
+	// Implement the SGD training logic for a batch
+	// ... (details omitted)
+}
 
-	// Use Sendfile to transfer the file
-	_, err = syscall.Sendfile(int(connFile.Fd()), int(file.Fd()), nil, fileSize)
-	if err != nil {
-		fmt.Println("Error transferring file:", err)
-		return
-	}
-
-	fmt.Println("File transfer completed successfully.")
+func TestSGD(t *testing.T) {
+	// Assert that SGD trains the MLP with reasonable accuracy
+	mlp := NewMLP(2, 3, 1, ReLU)
+	trainingData := [][]float64{{1, 1}, {0, 1}, {1, 0}, {0, 0}}
+	targets := []float64{1, 1, 0, 0}
+	mlp.SGD(trainingData, targets, 0.01, 100, 1)
+	// Perform additional assertions to evaluate the accuracy
+	// ...
+}
+func TestSGD(t *testing.T) {
+	// Assert that SGD trains the MLP with reasonable accuracy
+	mlp := NewMLP(2, 3, 1, ReLU)
+	trainingData := [][]float64{{1, 1}, {0, 1}, {1, 0}, {0, 0}}
+	targets := []float64{1, 1, 0, 0}
+	mlp.SGD(trainingData, targets, 0.01, 100, 1)
+	// Perform additional assertions to evaluate the accuracy
+	// ...
 }
