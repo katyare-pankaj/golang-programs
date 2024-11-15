ad7b3f0a676160be79cce388f178d85093eb2f6a
pankaj-solanki1 <katyarepankaj31@gmail.com>
2024-11-15 22:10:35 +0530

    106215

--- RLHF/golang_alignment_5/15-11-24/106215/turn2/modelA/main.go
+++ RLHF/golang_alignment_5/15-11-24/106215/turn2/modelA/main.go
@@ -1,76 +1,81 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
 	"fmt"
-	"runtime"
+	"log"
 	"time"
 )
 
-var total int
-
-func naiveFactorial(n int) int {
-	if n == 0 || n == 1 {
-		return 1
-	}
-	return n * naiveFactorial(n-1)
-}
-func iterativeFactorial(n int) int {
-	result := 1
-	for i := 1; i <= n; i++ {
-		result *= i
-	}
-	return result
-}
-func primeSieve(n int) ([]int, bool) {
-	isPrime := make([]bool, n+1)
-	primes := []int{}
-
-	for i := 2; i <= n; i++ {
-		if isPrime[i] {
-			primes = append(primes, i)
-			for j := i * i; j <= n; j += i {
-				isPrime[j] = false
-			}
-		}
-	}
-	return primes, true
-}
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	startTime := time.Now()
-	func1()
-	endTime := time.Now()
-	duration := endTime.Sub(startTime)
-	fmt.Printf("Execution duration of functin1:%s \n", duration)
+	// Generate a new RSA key pair for PartyA
+	partyAPrivateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	startTime = time.Now()
-	func2()
-	endTime = time.Now()
-	duration = endTime.Sub(startTime)
-	fmt.Printf("Execution duration of func2:%s\n", duration)
+	// Generate a new RSA key pair for PartyB
+	partyBPrivateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	// Create a simple self-signed certificate for PartyA
+	partyACert, err := createSelfSignedCert(partyAPrivateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a simple self-signed certificate for PartyB
+	partyBCert, err := createSelfSignedCert(partyBPrivateKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// PartyA encrypts a message for PartyB using PartyB's public key
+	message := []byte("Hello, PartyB! This is a secure message from PartyA.")
+	encryptedMessage, err := encryptMessage(partyBPublicKey(partyBCert), message)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// PartyB receives the encrypted message and decrypts it using its private key
+	decryptedMessage, err := decryptMessage(partyBPrivateKey, encryptedMessage)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("PartyA sent:", string(message))
+	fmt.Println("PartyB received:", string(decryptedMessage))
 }
 
-func func1() {
-
-	/* This is to  See the common tail call optimization ; in goc compilation of default of this is 1}*,Hence,  no real impact is seen directly here.*/
-
-	for i := 0; i < 10000; i++ {
-		fmt.Println(naiveFactorial(15))
-		total++
+func createSelfSignedCert(privateKey *rsa.PrivateKey) (*x509.Certificate, error) {
+	template := &x509.Certificate{
+		SerialNumber: 1,
+		Subject:      x509.Name{CommonName: "PartyA"},
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().Add(time.Year),
+		KeyUsage:     x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
+		BasicConstraints: &x509.BasicConstraints{
+			IsCA:       false,
+			MaxPathLen: nil,
+		},
 	}
+
+	return x509.CreateCertificate(rand.Reader, template, template, privateKey, privateKey)
 }
 
-func func2() {
-	/*argument starts at  i from the local higher function,identifier seekar into the samefunction; hence SROA suffers here, but goo recovery wush loo read
-	 */
+func partyBPublicKey(cert *x509.Certificate) *rsa.PublicKey {
+	return cert.PublicKey.(*rsa.PublicKey)
+}
 
-	primes, okay := primeSieve(100000)
-	if !okay {
-		panic("Failed to sieve primes")
-	}
-	for i := range primes {
-		total += iterativeFactorial(primes[i])
-	}
+func encryptMessage(publicKey *rsa.PublicKey, message []byte) ([]byte, error) {
+	return rsa.EncryptOAEP(rand.Reader, nil, publicKey, message, []byte("example")) // Replace "example" with a secure padding string
+}
 
+func decryptMessage(privateKey *rsa.PrivateKey, encryptedMessage []byte) ([]byte, error) {
+	return rsa.DecryptOAEP(rand.Reader, nil, privateKey, encryptedMessage, []byte("example")) // Replace "example" with the same secure padding string
 }
--- RLHF/golang_alignment_5/15-11-24/106215/turn2/modelB/main.go
+++ RLHF/golang_alignment_5/15-11-24/106215/turn2/modelB/main.go
@@ -1,46 +1,105 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"fmt"
-	"runtime"
+	"log"
+	"net/http"
 	"time"
 )
 
-func quicksort(arr []int, low, high int) {
-	if low < high {
-		pi := partition(arr, low, high)
-		quicksort(arr, low, pi-1)
-		quicksort(arr, pi+1, high)
-	}
-}
-
-func partition(arr []int, low, high int) int {
-	pivot := arr[high]
-	i := low - 1
-	for j := low; j < high; j++ {
-		if arr[j] <= pivot {
-			i++
-			arr[i], arr[j] = arr[j], arr[i]
-		}
-	}
-	arr[i+1], arr[high] = arr[high], arr[i+1]
-	return i + 1
-}
-
 func main() {
-	runtime.GOMAXPROCS(1) // Set GOMAXPROCS to 1 for single-threaded execution
-	const size = 100000
-	arr := make([]int, size)
-
-	// Generate random array
-	for i := range arr {
-		arr[i] = size - i
+	// Generate a self-signed certificate for the server
+	serverCert, serverKey, err := generateCertificate("server.local")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	start := time.Now()
-	quicksort(arr, 0, size-1)
-	elapsed := time.Since(start)
+	// Create a TLS config with the server certificate and key
+	serverTLSConfig := &tls.Config{
+		Certificates: []tls.Certificate{{
+			Certificate: serverCert,
+			PrivateKey:  serverKey,
+		}},
+		ClientAuth: tls.RequestClientCert,
+	}
 
-	fmt.Printf("Sorted array: %v\n", arr)
-	fmt.Printf("Time elapsed: %s\n", elapsed)
+	// Request client certificates and verify them
+	serverTLSConfig.ClientCAs = x509.NewCertPool()
+	pemCABytes := []byte(`-----BEGIN CERTIFICATE-----
+MIICmzCCAh8CAQAwDQYJKoZIhvcNAQELBQAwFjEUMBIGA1UEAxMLc2VydmVyLmxv
+Y2FsMB4XDTIzMDcxNDE2MjM1OFoXDTI0MDcxNDE2MjM1OFowFjEUMBIGA1UEAxML
+c2VydmVyLmxvY2FsMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtH/s
+2y795z7MhgZm9o7FbXqK/JHv2G2EbePd0Gc5pXq36z/Rrl24dF0o3y3G30Ht4Jdq
+VK8930Vd6F0/3Pqxz24z34LzXtN57+9B0z+YZBwf9206R0x50SQ7i8BJnK3Z63Bf
+Hm0y4TbE1N1Q5Np1PQ7hG4JW5sNxGj/ZU9F8yzw63BXF9+4QlY+iBZ3Lnw1v+Kqw
+vAqYsT1Xhj+d03SX9yp4hGgxWW0e23qk7ZiGjA7z0rBqjTjOv5GzO67OqbNwkW+L
+wM+Uow9i+22Dny5w4e4zpS85x11lRqd7H1XvZbJh/H6xE+68EwIDAQABo4IBqzCC
+AacwDgYDVR0PAQH/BAQDAgeAMBMGA1UdJQQMMAoGA1UdAgQGMA8wHQYDVR0OBBYE
+FITsF2x0bUa7XtB1vhOgvG3Hr7W0MAkGA1UdEwQCMAAwHwYDVR0jBBgwFoAUhOwX
+bHVtRrtelHW+E6C8bcets7QwEwYDVR0lBAwwCgYEVQUDBAYwFjEUMBIGA1UEAxML
+c2VydmVyLmxvY2FsMB8GA1UdIwQYMBYGFBRELBdsdG1Gu17Qdb4ToLxtx6+1tDAd
+BgNVHSUEFjAUBggrBgEFBQcDAQYIKwYBBQUHAwIwGQYDVR0RBBQwEoIMc2VydmVy
+LmxvY2FsMIIBDAKBgQC/b/xMv/XzjzB6CcJXZ+g/5694L5EJxjV83L1v+yWr7/UW
+7X4v5cXH8b63n0Z0BxVg24kZv/fQ2H9ZcPk3iXjV8602P4vjECcJrRbEGzYUzUi5
+Y0HpN9w5i8R20rI+X/Xy01V8v3x4Hx3NzTR3Bp4QP7Gh81R2T6Z8Z/T4k9Xe6C5H
+tAIBADAOBgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUDAgEGMB0GA1UdDgQWBBSE
+7BdsdG1Gu17Qdb4ToLxtx6+1tDAPBgNVHRMBAf8EBTADAQH/MAkGA1UdEwQCMAAw
+-----END CERTIFICATE-----`)
+	if ok := serverTLSConfig.ClientCAs.AppendCertsFromPEM(pemCABytes); !ok {
+		log.Fatal("Failed to add client CA certificate")
+	}
+
+	// Serve the HTTP handler with TLS
+	server := &http.Server{
+		Addr:      ":8443",
+		Handler:   http.HandlerFunc(helloHandler),
+		TLSConfig: serverTLSConfig,
+	}
+
+	log.Println("Server listening on 8443")
+	if err := server.ListenAndServeTLS("", ""); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, client! You are authenticated: %v\n", r.TLS.PeerCertificates)
+}
+
+func generateCertificate(commonName string) ([][]byte, []byte, error) {
+	// Generate a new RSA key pair
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Create a new certificate template
+	template := x509.Certificate{
+		SerialNumber: 1,
+		Subject:      pkix.Name{CommonName: commonName},
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().Add(1 * time.Year),
+		KeyUsage:     x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
+		BasicConstraints: x509.BasicConstraints{
+			CA: false,
+		},
+	}
+
+	// Sign the certificate with the private key
+	derCertificate, err := x509.CreateCertificate(rand.Reader, &template, &template, &privateKey.PublicKey, privateKey)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Return the certificate in PEM format
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: derCertificate})
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(privateKey)})
+	return []byte{certPEM}, keyPEM, nil
 }
