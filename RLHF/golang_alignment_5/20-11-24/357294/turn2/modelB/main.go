4bf6e617f40d0ffffe412af831e87df3c6950d67
pankaj-solanki1 <katyarepankaj31@gmail.com>
2024-11-21 00:51:45 +0530

    357294

--- RLHF/golang_alignment_5/20-11-24/357294/turn2/modelB/main.go
+++ RLHF/golang_alignment_5/20-11-24/357294/turn2/modelB/main.go
@@ -1,41 +1,56 @@
 package main
 
 import (
-	"context"
+	"errors"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
-	"github.com/google/uuid"
-	"github.com/pkg/errors"
+	"golang.org/x/exp/rand"
 )
 
-// Mock blocking library function
-func blockingLibraryOperation(ctx context.Context, id uuid.UUID) (string, error) {
-	select {
-	case <-ctx.Done():
-		return "", errors.Wrap(ctx.Err(), "operation canceled")
-	default:
-		// Simulate a blocking operation that takes 2 seconds
-		time.Sleep(2 * time.Second)
-		return fmt.Sprintf("Result for %s", id), nil
+func main() {
+	task := func() error {
+		// Simulate a task that might fail sometimes
+		time.Sleep(time.Duration(1+rand.Intn(2)) * time.Second)
+		if rand.Intn(2) == 0 {
+			return errors.New("task failed randomly")
+		}
+		fmt.Println("Task executed successfully.")
+		return nil
+	}
+
+	errChan := make(chan error)
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
+
+	go scheduleTask(task, 3*time.Second, errChan)
+
+	for {
+		select {
+		case err := <-errChan:
+			fmt.Println("Received error:", err)
+		case <-stopChan:
+			fmt.Println("Stopping scheduler...")
+			return
+		}
 	}
 }
 
-func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-	defer cancel()
+func scheduleTask(task func() error, interval time.Duration, errChan chan<- error) {
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
 
-	id := uuid.New()
-	go func() {
-		result, err := blockingLibraryOperation(ctx, id)
-		if err != nil {
-			fmt.Println("Error:", err)
-		} else {
-			fmt.Println("Result:", result)
+	for {
+		select {
+		case <-ticker.C:
+			if err := task(); err != nil {
+				errChan <- err
+			}
+		case <-time.After(interval * 2):
+			errChan <- errors.New("task execution timed out")
 		}
-	}()
-
-	// Do other work in the main goroutine while the blocking operation is in progress
-	time.Sleep(100 * time.Millisecond)
-	fmt.Println("Main goroutine doing other work.")
+	}
 }
