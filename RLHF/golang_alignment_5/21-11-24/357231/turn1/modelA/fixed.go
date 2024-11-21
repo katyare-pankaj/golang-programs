0410b5f98d090e10dcd368d0459c5ad172b0ca77
pankaj-solanki1 <katyarepankaj31@gmail.com>
2024-11-21 20:04:00 +0530

    357308

--- RLHF/golang_alignment_5/21-11-24/357231/turn1/modelA/fixed.go
+++ RLHF/golang_alignment_5/21-11-24/357231/turn1/modelA/fixed.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"time"
+)
+
+func busyLoop() {
+	for {
+	}
+}
+
+func main() {
+	go busyLoop()               // Simulating some work
+	time.Sleep(time.Second * 5) // Sleep for 5 seconds to allow profiling
+
+	fmt.Println("Starting HTTP server for profiling...")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Profiling available at /debug/pprof/")
+	})
+
+	log.Fatal(http.ListenAndServe(":6060", nil))
+}
--- RLHF/golang_alignment_5/21-11-24/357231/turn1/modelB/main.go
+++ RLHF/golang_alignment_5/21-11-24/357231/turn1/modelB/main.go
@@ -1,55 +1,31 @@
-// main.go
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-
-	httptransport "github.com/go-swagger/go-swagger/httpkit/transport"
-	"github.com/gorilla/mux"
+	"math/rand"
+	"os"
+	"runtime/pprof"
+	"time"
 )
 
-// Todo struct defines the structure of a to-do item
-type Todo struct {
-	ID    int    `json:"id"`
-	Title string `json:"title"`
-	Done  bool   `json:"done"`
-}
-
-// ToDos array of Todo struct
-type ToDos []Todo
-
-// todosMap is a simple in-memory map to store to-do items (for demonstration purposes)
-var todosMap map[int]Todo
-
-func init() {
-	todosMap = make(map[int]Todo)
-	// Add some sample to-do items for demonstration
-	todosMap[1] = Todo{ID: 1, Title: "Buy milk", Done: false}
-	todosMap[2] = Todo{ID: 2, Title: "Learn Go", Done: true}
-}
-
 func main() {
-	r := mux.NewRouter()
-	api := NewAPI(swaggerSpec())
-
-	// Register the API routes
-	r.Handle("/", httptransport.New(api, api.GetTodosHandler, api.GetTodosConsumer))
-	r.Handle("/{id}", httptransport.New(api, api.GetTodoHandler, api.GetTodoConsumer))
-	r.Handle("/", httptransport.New(api, api.CreateTodoHandler, api.CreateTodoConsumer))
-	r.Handle("/{id}", httptransport.New(api, api.UpdateTodoHandler, api.UpdateTodoConsumer))
-	r.Handle("/{id}", httptransport.New(api, api.DeleteTodoHandler, api.DeleteTodoConsumer))
-
-	fmt.Println("Server started on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
-}
-
-// swaggerSpec retrieves the swagger spec from a local file
-func swaggerSpec() *Spec {
-	spec, err := loadSpec("swagger.yaml")
+	// Start CPU profiling
+	f, err := os.Create("cpu.prof")
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
-	return spec
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
+
+	// Simulate some work
+	for i := 0; i < 1000000; i++ {
+		time.Sleep(time.Duration(rand.Intn(10)) * time.Microsecond)
+	}
+
+	// Start memory profiling
+	mf, err := os.Create("mem.prof")
+	if err != nil {
+		panic(err)
+	}
+	defer mf.Close()
+	pprof.WriteHeapProfile(mf)
 }
