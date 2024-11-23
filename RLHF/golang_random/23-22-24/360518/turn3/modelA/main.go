cb3ca1d3175b4a664954f7feba2a6221bd8cd4fb
pankaj-solanki1 <katyarepankaj31@gmail.com>
2024-11-23 18:11:14 +0530

    360518

--- RLHF/golang_random/23-22-24/360518/turn3/modelA/main.go
+++ RLHF/golang_random/23-22-24/360518/turn3/modelA/main.go
@@ -2,149 +2,88 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
-// Vertex interface defines the operations that a vertex must support.
-type Vertex interface {
-	GetID() int
-	SetID(int)
-	AddEdge(Edge)
-	GetEdges() []Edge
-	RemoveEdge(Edge)
+const (
+	maxTasksPerSecond = 5 // Set the rate limit to 5 tasks per second
+	tokenBucketSize   = maxTasksPerSecond
+	resetInterval     = time.Second
+)
+
+type Task struct {
+	ID   int
+	Data string
 }
 
-// Edge interface defines the operations that an edge must support.
-type Edge interface {
-	GetSource() Vertex
-	GetDestination() Vertex
-	SetWeight(int)
-	GetWeight() int
-}
-
-// Graph struct represents the graph with a map of vertices.
-type Graph struct {
-	vertices map[int]Vertex
-}
-
-func NewGraph() *Graph {
-	return &Graph{
-		vertices: make(map[int]Vertex),
-	}
-}
-
-func (g *Graph) AddVertex(vertex Vertex) {
-	g.vertices[vertex.GetID()] = vertex
-}
-
-func (g *Graph) GetVertex(id int) Vertex {
-	return g.vertices[id]
-}
-
-func (g *Graph) RemoveVertex(id int) {
-	delete(g.vertices, id)
-}
-
-func (g *Graph) TraverseGraph() {
-	for _, vertex := range g.vertices {
-		fmt.Printf("Vertex %d:\n", vertex.GetID())
-		for _, edge := range vertex.GetEdges() {
-			fmt.Printf("  -> Vertex %d (Weight: %d)\n", edge.GetDestination().GetID(), edge.GetWeight())
-		}
-	}
-}
-
-// Simple Vertex implementation with basic properties.
-type BasicVertex struct {
-	id    int
-	edges []Edge
-}
-
-func NewBasicVertex(id int) *BasicVertex {
-	return &BasicVertex{
-		id:    id,
-		edges: nil,
-	}
-}
-
-func (v *BasicVertex) GetID() int {
-	return v.id
-}
-
-func (v *BasicVertex) SetID(id int) {
-	v.id = id
-}
-
-func (v *BasicVertex) AddEdge(edge Edge) {
-	v.edges = append(v.edges, edge)
-}
-
-func (v *BasicVertex) GetEdges() []Edge {
-	return v.edges
-}
-
-func (v *BasicVertex) RemoveEdge(edge Edge) {
-	for i, e := range v.edges {
-		if e == edge {
-			v.edges = append(v.edges[:i], v.edges[i+1:]...)
-			return
-		}
-	}
-}
-
-// Simple Edge implementation with basic properties.
-type BasicEdge struct {
-	source      Vertex
-	destination Vertex
-	weight      int
-}
-
-func NewBasicEdge(source Vertex, destination Vertex, weight int) *BasicEdge {
-	return &BasicEdge{
-		source:      source,
-		destination: destination,
-		weight:      weight,
-	}
-}
-
-func (e *BasicEdge) GetSource() Vertex {
-	return e.source
-}
-
-func (e *BasicEdge) GetDestination() Vertex {
-	return e.destination
-}
-
-func (e *BasicEdge) SetWeight(weight int) {
-	e.weight = weight
-}
-
-func (e *BasicEdge) GetWeight() int {
-	return e.weight
-}
+var (
+	taskProcessorCh = make(chan Task) // Channel to process tasks
+	wg              sync.WaitGroup
+	tokenBucket     = make(chan struct{}, tokenBucketSize)
+	taskCounter     int
+)
 
 func main() {
-	graph := NewGraph()
+	// Start a goroutine to refill the token bucket every second
+	go refillTokenBucket()
 
-	vertex1 := NewBasicVertex(1)
-	vertex2 := NewBasicVertex(2)
-	vertex3 := NewBasicVertex(3)
+	// Start goroutines to process tasks from different sources
+	go processTasks()
 
-	edge12 := NewBasicEdge(vertex1, vertex2, 5)
-	edge13 := NewBasicEdge(vertex1, vertex3, 3)
-	edge23 := NewBasicEdge(vertex2, vertex3, 4)
+	// Process tasks from multiple sources
+	go handleTasksFromSource1()
+	go handleTasksFromSource2()
 
-	graph.AddVertex(vertex1)
-	graph.AddVertex(vertex2)
-	graph.AddVertex(vertex3)
+	// Wait for all tasks to be processed
+	wg.Wait()
+}
 
-	vertex1.AddEdge(edge12)
-	vertex1.AddEdge(edge13)
-	vertex2.AddEdge(edge23)
+func refillTokenBucket() {
+	ticker := time.NewTicker(resetInterval)
+	defer ticker.Stop()
 
-	fmt.Println("Graph Traversal:")
-	graph.TraverseGraph()
+	for range ticker.C {
+		// Reset the token bucket to the maximum size
+		for i := 0; i < tokenBucketSize; i++ {
+			select {
+			case tokenBucket <- struct{}{}:
+			default:
+				// Bucket is full
+				return
+			}
+		}
+	}
+}
 
-	fmt.Println("\nRemoving vertex 3:")
-	graph.RemoveVertex(vertex3.GetID())
-	graph.TraverseGraph()
+func processTasks() {
+	for task := range taskProcessorCh {
+		// Take a token to process the task
+		<-tokenBucket
+
+		// Simulate processing the task
+		fmt.Printf("Processing Task %d: %s\n", task.ID, task.Data)
+		time.Sleep(time.Millisecond * 500) // Simulate task processing time
+
+		// Return the token
+		tokenBucket <- struct{}{}
+	}
+}
+
+func handleTasksFromSource1() {
+	for i := 1; i <= 10; i++ {
+		// Send a task from source 1
+		task := Task{ID: i, Data: fmt.Sprintf("Task from Source1 - %d", i)}
+		taskProcessorCh <- task
+	}
+	wg.Done()
+}
+
+func handleTasksFromSource2() {
+	for i := 11; i <= 20; i++ {
+		// Send a task from source 2
+		task := Task{ID: i, Data: fmt.Sprintf("Task from Source2 - %d", i)}
+		taskProcessorCh <- task
+	}
+	wg.Done()
 }
--- RLHF/golang_random/23-22-24/360518/turn3/modelB/main.go
+++ RLHF/golang_random/23-22-24/360518/turn3/modelB/main.go
@@ -2,134 +2,67 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-type VertexID int
-type Vertex interface {
-	GetID() VertexID
-	AddEdge(Edge)
-	GetEdges() []Edge
-	RemoveEdge(VertexID)
+type Task struct {
+	ID     int
+	Data   string
+	Source int
 }
-type Edge interface {
-	GetSourceID() VertexID
-	GetDestinationID() VertexID
-	GetWeight() int
-}
-type Graph interface {
-	AddVertex(Vertex)
-	RemoveVertex(VertexID)
-	GetVertex(VertexID) Vertex
-	HasVertex(VertexID) bool
-	IterateVertices(func(Vertex))
-}
-type BasicVertex struct {
-	id    VertexID
-	edges map[VertexID]Edge
+type RateLimiter struct {
+	burst  int
+	period time.Duration
+	ch     chan Task
 }
 
-func NewBasicVertex(id VertexID) Vertex {
-	return &BasicVertex{
-		id:    id,
-		edges: make(map[VertexID]Edge),
+func NewRateLimiter(burst int, period time.Duration) *RateLimiter {
+	return &RateLimiter{
+		burst:  burst,
+		period: period,
+		ch:     make(chan Task, burst),
 	}
 }
-func (v *BasicVertex) GetID() VertexID {
-	return v.id
-}
-func (v *BasicVertex) AddEdge(edge Edge) {
-	v.edges[edge.GetDestinationID()] = edge
-}
-func (v *BasicVertex) GetEdges() []Edge {
-	edges := make([]Edge, 0, len(v.edges))
-	for _, edge := range v.edges {
-		edges = append(edges, edge)
+func (rl *RateLimiter) ProcessTasks() {
+	ticker := time.NewTicker(rl.period)
+	defer ticker.Stop()
+	for {
+		select {
+		case task := <-rl.ch:
+			fmt.Printf("Processing Task %d: %s (Source: %d)\n", task.ID, task.Data, task.Source)
+			time.Sleep(time.Second) // Simulate task processing time
+		case <-ticker.C:
+			for len(rl.ch) > 0 {
+				task := <-rl.ch
+				fmt.Printf("Processing Task %d: %s (Source: %d)\n", task.ID, task.Data, task.Source)
+				time.Sleep(time.Second) // Simulate task processing time
+			}
+		}
 	}
-	return edges
 }
-func (v *BasicVertex) RemoveEdge(destID VertexID) {
-	delete(v.edges, destID)
+func GenerateTasks(source int, out chan<- Task, rateLimiter *RateLimiter) {
+	taskID := 1
+	for {
+		task := Task{ID: taskID, Data: fmt.Sprintf("Task from source %d", source), Source: source}
+		rateLimiter.ch <- task
+		taskID++
+	}
 }
 
-type BasicEdge struct {
-	sourceID      VertexID
-	destinationID VertexID
-	weight        int
-}
-
-func NewBasicEdge(sourceID, destinationID VertexID, weight int) Edge {
-	return &BasicEdge{
-		sourceID:      sourceID,
-		destinationID: destinationID,
-		weight:        weight,
-	}
-}
-func (e *BasicEdge) GetSourceID() VertexID {
-	return e.sourceID
-}
-func (e *BasicEdge) GetDestinationID() VertexID {
-	return e.destinationID
-}
-func (e *BasicEdge) GetWeight() int {
-	return e.weight
-}
-
-type AdjacencyListGraph struct {
-	vertices map[VertexID]Vertex
-}
-
-func NewAdjacencyListGraph() Graph {
-	return &AdjacencyListGraph{
-		vertices: make(map[VertexID]Vertex),
-	}
-}
-func (g *AdjacencyListGraph) AddVertex(vertex Vertex) {
-	g.vertices[vertex.GetID()] = vertex
-}
-func (g *AdjacencyListGraph) RemoveVertex(vertexID VertexID) {
-	delete(g.vertices, vertexID)
-	// Remove edges incident on this vertex
-	for _, vertex := range g.vertices {
-		vertex.RemoveEdge(vertexID)
-	}
-}
-func (g *AdjacencyListGraph) GetVertex(vertexID VertexID) Vertex {
-	return g.vertices[vertexID]
-}
-func (g *AdjacencyListGraph) HasVertex(vertexID VertexID) bool {
-	_, ok := g.vertices[vertexID]
-	return ok
-}
-func (g *AdjacencyListGraph) IterateVertices(iterate func(Vertex)) {
-	for _, vertex := range g.vertices {
-		iterate(vertex)
-	}
-}
 func main() {
-	graph := NewAdjacencyListGraph()
-	// Add vertices dynamically
-	graph.AddVertex(NewBasicVertex(1))
-	graph.AddVertex(NewBasicVertex(2))
-	graph.AddVertex(NewBasicVertex(3))
-	// Add edges
-	graph.GetVertex(1).AddEdge(NewBasicEdge(1, 2, 5))
-	graph.GetVertex(1).AddEdge(NewBasicEdge(1, 3, 3))
-	graph.GetVertex(2).AddEdge(NewBasicEdge(2, 3, 4))
-	// Traverse the graph
-	fmt.Println("Graph Traversal:")
-	graph.IterateVertices(func(vertex Vertex) {
-		fmt.Printf("Vertex %d:\n", vertex.GetID())
-		for _, edge := range vertex.GetEdges() {
-			fmt.Printf("  -> Vertex %d (Weight: %d)\n", edge.GetDestinationID(), edge.GetWeight())
-		}
-	})
-	// Remove a vertex
-	graph.RemoveVertex(3)
-	fmt.Println("\nGraph after removing vertex 3:")
-	graph.IterateVertices(func(vertex Vertex) {
-		fmt.Printf("Vertex %d:\n", vertex.GetID())
-		for _, edge := range vertex.GetEdges() {
-			fmt.Printf("  -> Vertex %d (Weight: %d)\n", edge.GetDestinationID(), edge.GetWeight())
-		}
-	})
+	// Create a rate limiter with a burst of 2 tasks and a period of 2 seconds
+	rateLimiter := NewRateLimiter(2, 2*time.Second)
+
+	// Start the rate limiter
+	go rateLimiter.ProcessTasks()
+
+	// Generate tasks from different sources (channels)
+	taskChannel1 := make(chan Task)
+	go GenerateTasks(1, taskChannel1, rateLimiter)
+	taskChannel2 := make(chan Task)
+	go GenerateTasks(2, taskChannel2, rateLimiter)
+
+	// Simulate processing tasks for 10 seconds
+	time.Sleep(10 * time.Second)
+	fmt.Println("Task processing completed.")
 }
