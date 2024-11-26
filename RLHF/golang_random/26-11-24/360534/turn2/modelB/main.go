9034d20fd14b5d34e8e1f2c93bfedc826c88cd72
pankaj-solanki1 <katyarepankaj31@gmail.com>
2024-11-26 21:45:39 +0530

    360534

--- RLHF/golang_random/26-11-24/360534/turn2/modelB/main.go
+++ RLHF/golang_random/26-11-24/360534/turn2/modelB/main.go
@@ -1,22 +1,54 @@
-package main  
-  
-import (  
-    "flag"  
-    "fmt"
-)  
-  
-var username = flag.String("username", "", "Username")  
-var password = flag.String("password", "", "Password")  
-  
-func main() {  
-    flag.Parse()  
+package main
 
-    correctUsername := "myuser"
-    correctPassword := "mypassword"
+import (
+	"fmt"
+	"sync"
+)
 
-    if *username == correctUsername && *password == correctPassword {
-        fmt.Println("Login successful")  
-    } else {  
-        fmt.Println("Invalid credentials")  
-    }  
-}  
+func generateData(dataCh chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 1; i <= 100; i++ {
+		dataCh <- i
+	}
+	close(dataCh)
+}
+
+func transformData(dataCh <-chan int, transformedCh chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for data := range dataCh {
+		transformedCh <- data + 1
+	}
+	close(transformedCh)
+}
+
+func aggregateResults(transformedCh <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var total int
+	for transformed := range transformedCh {
+		total += transformed
+	}
+	fmt.Println("Final Sum:", total)
+}
+
+func main() {
+	var wg sync.WaitGroup
+	dataCh := make(chan int)
+	transformedCh := make(chan int)
+
+	// Launch the data generators
+	wg.Add(1)
+	go generateData(dataCh, &wg)
+
+	// Launch the data transformers
+	const numTransformers = 4
+	wg.Add(numTransformers)
+	for i := 0; i < numTransformers; i++ {
+		go transformData(dataCh, transformedCh, &wg)
+	}
+
+	// Launch the aggregator
+	wg.Add(1)
+	go aggregateResults(transformedCh, &wg)
+
+	wg.Wait()
+}
