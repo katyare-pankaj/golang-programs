2c8d2545824567435d9bb4b81a8a6ac7cdc6fd89
pankaj-solanki1 <katyarepankaj31@gmail.com>
2024-11-29 00:31:47 +0530

    389166

--- RLHF/golang_random/28-11-24/389166/turn2/modelA/main.go
+++ RLHF/golang_random/28-11-24/389166/turn2/modelA/main.go
@@ -2,22 +2,48 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
+var wg sync.WaitGroup
+
+func task1() {
+	defer wg.Done()
+	fmt.Println("Started task 1")
+	time.Sleep(1 * time.Second)
+	fmt.Println("Finished task 1")
+}
+
+func task2() {
+	defer wg.Done()
+	fmt.Println("Started task 2")
+	time.Sleep(1 * time.Second)
+	fmt.Println("Finished task 2")
+}
+
+func task3() {
+	defer wg.Done()
+	fmt.Println("Started task 3")
+	time.Sleep(1 * time.Second)
+	fmt.Println("Finished task 3")
+}
+
+func task4() {
+	defer wg.Done()
+	fmt.Println("Started task 4")
+	time.Sleep(1 * time.Second)
+	fmt.Println("Finished task 4")
+}
+
 func main() {
-	// inconsistent indentation and line breaks
-	fmt.Println("Hello, World!")
-	a := 1
-	b := 2
-	c := 3
-	d := 4
-	// inconsistent spacing around operators
-	result := a + b*c - d/2
-	// inconsistent use of parentheses
-	fmt.Printf("Result: %d\n", result)
-	// inconsistent capitalization
-	str := "go"
-	// inconsistent variable declarations
-	var x int = 5
-	var y = 6
+	wg.Add(4) // Add the number of tasks to the wait group
+	go task1()
+	go task2()
+	go task3()
+	go task4()
+
+	// Wait for all tasks to complete
+	wg.Wait()
+	fmt.Println("All tasks have completed")
 }
--- RLHF/golang_random/28-11-24/389166/turn2/modelB/main.go
+++ RLHF/golang_random/28-11-24/389166/turn2/modelB/main.go
@@ -1,28 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
 
-// old.go
-func calculateSum(numbers []int) (sum int) {
-	for _, n := range numbers {
-		sum += n
-	}
-	return
-}
+var wg sync.WaitGroup
 
-func computeAverage(values []float64) float64 {
-	total := 0.0
-	for _, val := range values {
-		total += val
-	}
-	return total / float64(len(values))
+func processTask(taskId int, task chan int, results chan int) {
+	defer wg.Done()
+	// Simulate some processing time for each task
+	time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+
+	// Receive task from channel in the given order
+	taskIdToProcess := <-task
+
+	// Perform the actual task processing
+	result := taskIdToProcess * taskIdToProcess
+
+	// Send the result back using another channel to maintain order
+	results <- result
+	fmt.Printf("Processed task %d: %d\n", taskId, taskIdToProcess)
 }
 
 func main() {
+	tasks := []int{1, 2, 3, 4, 5}
+	numTasks := len(tasks)
 
-	sum := calculateSum([]int{1, 3, 4, 6, 7})
-	fmt.Println("Calculate sum of numbers :", sum)
+	// Channel to send tasks to worker goroutines in order
+	taskCh := make(chan int)
 
-	total := computeAverage([]float64{1.0, 3.0, 4.0, 6.0, 7.0})
-	fmt.Println("Calculate averate of numbers :", total)
+	// Channel to receive results from worker goroutines in order
+	resultCh := make(chan int)
+
+	// Start worker goroutines
+	wg.Add(numTasks)
+	for i := 0; i < numTasks; i++ {
+		go processTask(i+1, taskCh, resultCh)
+	}
+
+	// Send tasks to the worker goroutines in the correct order
+	for _, task := range tasks {
+		taskCh <- task
+	}
+
+	// Close the task channel to signal the workers that there are no more tasks
+	close(taskCh)
+
+	// Receive results from the result channel in the correct order and print them
+	for i := 0; i < numTasks; i++ {
+		result := <-resultCh
+		fmt.Printf("Result for task %d: %d\n", i+1, result)
+	}
+
+	wg.Wait()
+	close(resultCh)
 }
