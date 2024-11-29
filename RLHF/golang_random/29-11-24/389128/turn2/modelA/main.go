7699384084929f3a1481a66569994f438c37428e
pankaj-solanki1 <katyarepankaj31@gmail.com>
2024-11-29 23:45:13 +0530

    389128

--- RLHF/golang_random/29-11-24/389128/turn2/modelA/main.go
+++ RLHF/golang_random/29-11-24/389128/turn2/modelA/main.go
@@ -2,63 +2,35 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
+	"log"
+	"os"
+	"strconv"
 )
 
-type SharedResource struct {
-	data []int
-	rwmu sync.RWMutex
+// checkFeatureFlag reads an environment variable and checks if it is enabled.
+func checkFeatureFlag(flagName string) bool {
+	flagValue := os.Getenv(flagName)
+	if flagValue == "" {
+		return false // Feature is disabled by default
+	}
+
+	enabled, err := strconv.ParseBool(flagValue)
+	if err != nil {
+		log.Fatalf("Invalid value for environment variable %s: %v", flagName, err)
+	}
+
+	return enabled
 }
 
-func (s *SharedResource) Read() {
-	s.rwmu.RLock()         // Acquire a read lock
-	defer s.rwmu.RUnlock() // Release the read lock
-	fmt.Println("Current data:", s.data)
-}
-
-func (s *SharedResource) Write(newData []int) {
-	s.rwmu.Lock()         // Acquire a write lock
-	defer s.rwmu.Unlock() // Release the write lock
-	s.data = newData
-	fmt.Println("Data updated to:", s.data)
+// newFeatureDemo performs operations specific to the new feature.
+func newFeatureDemo() {
+	fmt.Println("Running the new feature functionality.")
 }
 
 func main() {
-	var sr SharedResource
-	sr.data = []int{1, 2, 3, 4, 5}
-	const numReadThreads = 10
-	const numWriteThreads = 2
-	const numIterations = 10
-
-	// Function for a reader thread
-	readThread := func() {
-		for i := 0; i < numIterations; i++ {
-			sr.Read()
-			time.Sleep(100 * time.Microsecond)
-		}
+	if checkFeatureFlag("NEW_FEATURE_ENABLED") {
+		newFeatureDemo()
+	} else {
+		log.Println("New feature is disabled.")
 	}
-
-	// Function for a writer thread
-	writeThread := func() {
-		for i := 0; i < numIterations; i++ {
-			newData := sr.data[:]
-			newData[0] += i + 1
-			sr.Write(newData)
-			time.Sleep(500 * time.Microsecond)
-		}
-	}
-
-	// Start reader threads
-	for i := 0; i < numReadThreads; i++ {
-		go readThread()
-	}
-
-	// Start writer threads
-	for i := 0; i < numWriteThreads; i++ {
-		go writeThread()
-	}
-
-	// Wait for all threads to complete
-	time.Sleep(2 * time.Second)
 }
--- RLHF/golang_random/29-11-24/389128/turn2/modelB/main.go
+++ RLHF/golang_random/29-11-24/389128/turn2/modelB/main.go
@@ -2,64 +2,21 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"sync"
+	"os"
 )
 
-// SharedData represents our shared resource
-type SharedData struct {
-	data    map[int]int
-	rwMutex sync.RWMutex
-}
-
-func newSharedData() *SharedData {
-	return &SharedData{
-		data: make(map[int]int),
-	}
-}
-
-// Read access, multiple readers allowed
-func (sd *SharedData) Read(key int) int {
-	sd.rwMutex.RLock()
-	defer sd.rwMutex.RUnlock()
-	return sd.data[key]
-}
-
-// Write access, only one writer allowed
-func (sd *SharedData) Write(key, value int) {
-	sd.rwMutex.Lock()
-	defer sd.rwMutex.Unlock()
-	sd.data[key] = value
-}
-
 func main() {
-	var wg sync.WaitGroup
-	const numThreads = 10
-	const numIterations = 10000
-	sharedData := newSharedData()
+	// Check if the "ENABLE_FEATURE" environment variable is set
+	enableFeature := os.Getenv("ENABLE_FEATURE") == "true"
 
-	wg.Add(numThreads)
-
-	// launching several readers
-	for i := 0; i < numThreads; i++ {
-		go func() {
-			for j := 0; j < numIterations; j++ {
-				key := rand.Intn(100)
-				value := sharedData.Read(key)
-				if value == 0 {
-					sharedData.Write(key, j) //writing new value
-				}
-			}
-			wg.Done()
-		}()
+	if enableFeature {
+		fmt.Println("Feature is enabled!")
+		doSomethingNew()
+	} else {
+		fmt.Println("Feature is disabled.")
 	}
-	wg.Wait()
+}
 
-	// Display the contents of the shared data
-	fmt.Println("Final Shared Data:")
-	sharedData.rwMutex.RLock()
-	for k, v := range sharedData.data {
-		fmt.Printf("%d: %d\n", k, v)
-	}
-	sharedData.rwMutex.RUnlock()
+func doSomethingNew() {
+	fmt.Println("Doing something new and exciting!")
 }
