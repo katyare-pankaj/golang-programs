d84219365721f9be4bec77cce53bee363dbcafa1
pankaj-solanki1 <katyarepankaj31@gmail.com>
2024-12-05 00:17:10 +0530

    390412

--- RLHF/golang_random/4-12-24/390412/turn2/modelB/main.go
+++ RLHF/golang_random/4-12-24/390412/turn2/modelB/main.go
@@ -2,42 +2,45 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
+	"runtime"
+	"sync"
 )
 
-// Function that could cause a panic
-func faultyFunction() {
-	// Example panic
-	panic("something went wrong!")
-}
-
-// Safeguarded function with logging and recovery
-func runSafely() {
-	defer func() {
-		if err := recover(); err != nil {
-			log.Printf("Recovered from panic: %v", err)
-			// Additional handling can be done here
-		}
-	}()
-
-	faultyFunction()
-	fmt.Println("Post-faultyFunction code that won't run if panicked.")
+func squareSum(start, end int, wg *sync.WaitGroup, resultChan chan<- int) {
+	defer wg.Done()
+	sum := 0
+	for i := start; i <= end; i++ {
+		sum += i * i
+	}
+	resultChan <- sum
 }
 
 func main() {
-	logFile, err := os.OpenFile("panic.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		fmt.Println("Failed to open log file:", err)
-		return
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	const n = 100000
+	const numGoroutines = 10
+	var wg sync.WaitGroup
+	resultChan := make(chan int, numGoroutines)
+
+	chunkSize := n / numGoroutines
+
+	wg.Add(numGoroutines)
+	for i := 0; i < numGoroutines; i++ {
+		start := i*chunkSize + 1
+		end := start + chunkSize - 1
+		if i == numGoroutines-1 {
+			end = n
+		}
+		go squareSum(start, end, &wg, resultChan)
 	}
-	defer logFile.Close()
 
-	log.SetOutput(logFile)
+	wg.Wait()
+	close(resultChan)
 
-	fmt.Println("Starting application...")
+	totalSum := 0
+	for result := range resultChan {
+		totalSum += result
+	}
 
-	runSafely()
-
-	fmt.Println("Application finished successfully.")
+	fmt.Printf("Total sum of squares: %d\n", totalSum)
 }
