f89e2006b55b7cb21892b12ad03af3f0ad98220a
pankaj-solanki1 <katyarepankaj31@gmail.com>
2024-12-24 23:23:58 +0530

    464950

--- RLHF/golang_random/24-12-24/464950/turn2/modelA/main.go
+++ RLHF/golang_random/24-12-24/464950/turn2/modelA/main.go
@@ -1,113 +1,47 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"sync"
-	"sync/atomic"
-	"time"
+	"io/ioutil"
+	"os"
+	"runtime/pprof"
 )
 
-// Configuration represents the configuration structure.
-type Configuration struct {
-	AppName string `json:"appName"`
-	Port    int    `json:"port"`
-}
-
-// ConfigManager represents the configuration manager.
-type ConfigManager struct {
-	config                    Configuration
-	isConfigLoaded            atomic.Bool
-	loadConfigWorkerDone      chan struct{}
-	ctx                       context.Context
-	cancel                    context.CancelFunc
-	loadDuration              time.Duration
-	loadConfigurationWorkerWG sync.WaitGroup
-}
-
-// NewConfigManager creates a new ConfigManager instance with specified load duration.
-func NewConfigManager(loadDuration time.Duration) *ConfigManager {
-	ctx, cancel := context.WithCancel(context.Background())
-	cm := &ConfigManager{
-		config:                    Configuration{},
-		loadConfigWorkerDone:      make(chan struct{}),
-		ctx:                       ctx,
-		cancel:                    cancel,
-		loadDuration:              loadDuration,
-		loadConfigurationWorkerWG: sync.WaitGroup{},
-	}
-	cm.loadConfigurationAsync()
-	return cm
-}
-
-// loadConfigurationAsync asynchronously loads the configuration.
-func (cm *ConfigManager) loadConfigurationAsync() {
-	defer cm.loadConfigurationWorkerWG.Done()
-
-	// Simulate the asynchronous load of configuration.
-	time.Sleep(cm.loadDuration)
-	config := Configuration{
-		AppName: "MyApp",
-		Port:    8080,
-	}
-	cm.setConfiguration(config)
-	close(cm.loadConfigWorkerDone)
-}
-
-// setConfiguration sets the configuration.
-func (cm *ConfigManager) setConfiguration(config Configuration) {
-	atomic.StoreBool(&cm.isConfigLoaded, true)
-	cm.config = config
-}
-
-// loadConfigurationBlocking blocks the caller until the configuration is fully loaded.
-func (cm *ConfigManager) loadConfigurationBlocking() {
-	select {
-	case <-cm.loadConfigWorkerDone:
-		// Configuration is already loaded.
-		return
-	case <-cm.ctx.Done():
-		// Context was canceled before load was complete.
-		fmt.Println("Configuration loading canceled")
-		return
-	default:
-		// Block until the load is complete.
-		<-cm.loadConfigWorkerDone
-	}
-}
-
-// GetConfiguration gets the loaded configuration.
-func (cm *ConfigManager) GetConfiguration() Configuration {
-	// Block until configuration is loaded if necessary.
-	cm.loadConfigurationBlocking()
-	return cm.config
-}
-
-// Close shuts down the configuration manager.
-func (cm *ConfigManager) Close() {
-	cm.cancel()
-	cm.loadConfigurationWorkerWG.Wait()
-}
-
 func main() {
-	cm := NewConfigManager(2 * time.Second)
 
-	// Simulate main application's startup code that needs configurations
-	fmt.Println("Starting application...")
-	time.Sleep(1 * time.Second)
+	// Create a file to save the CPU profile
+	cpuFile, err := os.Create("cpu.prof")
+	if err != nil {
+		fmt.Println("Error creating CPU profile:", err)
+		return
+	}
+	defer cpuFile.Close()
 
-	// Get configuration immediately and proceed with startup
-	config := cm.GetConfiguration()
-	if !cm.isConfigLoaded.Load() {
-		fmt.Println("Configuration not loaded yet. Aborting startup.")
-		cm.Close()
+	// Start the CPU profile
+	if err := pprof.StartCPUProfile(cpuFile); err != nil {
+		fmt.Println("Error starting CPU profile:", err)
+		return
+	}
+	defer pprof.StopCPUProfile()
+
+	f, err := os.CreateTemp("", "example")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer f.Close()
+
+	content := "Hello, World!"
+	if _, err := f.WriteString(content); err != nil {
+		fmt.Println("Error writing to file:", err)
 		return
 	}
 
-	// Proceed with application logic now that configuration is loaded
-	fmt.Println("Configuration loaded:", config)
-	fmt.Println("Application running with configuration:", config)
-	time.Sleep(5 * time.Second)
+	readContent, err := ioutil.ReadAll(f)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
 
-	cm.Close()
+	fmt.Println("Read content:", string(readContent))
 }
--- RLHF/golang_random/24-12-24/464950/turn2/modelB/main.go
+++ RLHF/golang_random/24-12-24/464950/turn2/modelB/main.go
@@ -2,83 +2,30 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
-type Configuration struct {
-	AppName string
-	Port    int
-}
-
-// ConfigManager represents the configuration manager.
-type ConfigManager struct {
-	config *Configuration
-	loaded bool
-	wg     sync.WaitGroup
-	lock   sync.Mutex
-	ch     chan struct{}
-}
-
-func NewConfigManager() *ConfigManager {
-	return &ConfigManager{
-		ch: make(chan struct{}),
-	}
-}
-
-// LoadConfigAsync initiates the asynchronous configuration loading process.
-func (m *ConfigManager) LoadConfigAsync() {
-	m.wg.Add(1)
-	go func() {
-		defer m.wg.Done()
-		time.Sleep(2 * time.Second) // Simulate loading time
-		config := &Configuration{
-			AppName: "MyApp",
-			Port:    8080,
-		}
-		m.lock.Lock()
-		m.config = config
-		m.loaded = true
-		m.lock.Unlock()
-		close(m.ch) // Signal configuration loaded
-	}()
-}
-
-// WaitForConfig blocks until the configuration is loaded and returns it.
-func (m *ConfigManager) WaitForConfig() *Configuration {
-	m.wg.Wait()
-	m.lock.Lock()
-	defer m.lock.Unlock()
-	// If the configuration is not loaded, wait for it to be loaded.
-	for !m.loaded {
-		<-m.ch
-	}
-	return m.config
-}
-
-// LoadConfig loads the configuration synchronously.
-func (m *ConfigManager) LoadConfig() *Configuration {
-	m.LoadConfigAsync()
-	return m.WaitForConfig()
-}
-
 func main() {
-	cm := NewConfigManager()
-	fmt.Println("Doing some work...")
+	start := time.Now()
+	defer fmt.Println("Total time:", time.Since(start))
 
-	// Load configuration asynchronously
-	cm.LoadConfigAsync()
-	fmt.Println("Asynchronous configuration loading initiated.")
+	// Run the benchmark function multiple times to get an average
+	const numBenchmarks = 1000000
+	for i := 0; i < numBenchmarks; i++ {
+		benchmarkDefer()
+	}
+}
 
-	// Other work can be done concurrently while configuration is being loaded.
-	time.Sleep(time.Second)
-	fmt.Println("Doing some more work...")
+func benchmarkDefer() {
+	// Simulate some work being done
+	for i := 0; i < 100; i++ {
+		_ = i * i
+	}
 
-	// Later, when needed, the application can block until the configuration is loaded using WaitForConfig.
-	config := cm.WaitForConfig()
-	fmt.Println("Using configuration:", config)
-
-	// Alternatively, load configuration synchronously
-	config = cm.LoadConfig()
-	fmt.Println("Using configuration (synchronously):", config)
+	// Benchmark the overhead of defer
+	defer func() {
+		// Simulate resource cleanup
+		// In reality, this could be anything like closing a file, releasing a lock, or logging.
+		_ = time.Sleep(time.Nanosecond * 1)
+	}()
 }
