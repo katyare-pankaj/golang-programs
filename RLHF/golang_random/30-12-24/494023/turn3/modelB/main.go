1a8dbe5b3904d977859b66891f728bf234e6a84d
pankaj-solanki1 <katyarepankaj31@gmail.com>
2024-12-30 21:13:17 +0530

    494023

--- RLHF/golang_random/30-12-24/494023/turn3/modelB/main.go
+++ RLHF/golang_random/30-12-24/494023/turn3/modelB/main.go
@@ -7,33 +7,55 @@ import (
 	"time"
 )
 
-func worker(wg *sync.WaitGroup, id int) {
-	defer wg.Done() // Decrement the WaitGroup counter when the goroutine exits.
+type inventory struct {
+	sync.Mutex
+	items map[string]int
+}
 
-	// Simulate work by sleeping for a random duration.
-	sleepDuration := time.Duration(rand.Intn(1000)) * time.Millisecond
-	fmt.Printf("Worker %d: Sleeping for %v...\n", id, sleepDuration)
-	time.Sleep(sleepDuration)
+func (i *inventory) updateInventory(item string, count int) {
+	i.Lock()
+	defer i.Unlock()
+	i.items[item] += count
+}
 
-	fmt.Printf("Worker %d: Done!\n", id)
+func (i *inventory) getInventory(item string) int {
+	i.Lock()
+	defer i.Unlock()
+	return i.items[item]
+}
+
+func (i *inventory) printInventory() {
+	i.Lock()
+	defer i.Unlock()
+	fmt.Println("Current Inventory:")
+	for item, count := range i.items {
+		fmt.Printf("%s: %d\n", item, count)
+	}
+}
+
+func simulateTransactions(inv *inventory) {
+	items := []string{"Apples", "Bananas", "Oranges", "Pineapples", "Mangoes"}
+	for {
+		item := items[rand.Intn(len(items))]
+		count := rand.Intn(10) - 5
+		inv.updateInventory(item, count)
+		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+	}
 }
 
 func main() {
-	var wg sync.WaitGroup
-	numWorkers := 5 // Number of worker goroutines to create.
+	inv := &inventory{items: make(map[string]int)}
+	inv.updateInventory("Apples", 100)
+	inv.updateInventory("Bananas", 80)
+	inv.updateInventory("Oranges", 60)
+	inv.updateInventory("Pineapples", 40)
+	inv.updateInventory("Mangoes", 20)
+	inv.printInventory()
 
-	// Add the number of worker goroutines to the WaitGroup.
-	wg.Add(numWorkers)
+	go simulateTransactions(inv)
+	go simulateTransactions(inv)
+	go simulateTransactions(inv)
 
-	// Start worker goroutines.
-	for i := 1; i <= numWorkers; i++ {
-		go worker(&wg, i)
-	}
-
-	fmt.Println("Main goroutine: Waiting for workers to finish...")
-
-	// Wait for all worker goroutines to call Done() on the WaitGroup.
-	wg.Wait()
-
-	fmt.Println("Main goroutine: All workers are done. Exiting...")
+	time.Sleep(30 * time.Second)
+	inv.printInventory()
 }
