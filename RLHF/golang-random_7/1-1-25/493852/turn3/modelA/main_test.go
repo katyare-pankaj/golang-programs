90556596ca4f4bbd6baf0b795453687d659441f9
pankaj-solanki1 <katyarepankaj31@gmail.com>
2025-01-01 19:51:10 +0530

    493852

---
+++ RLHF/golang-random_7/1-1-25/493852/turn3/modelA/main_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+const numStrings = 10000
+const strLength = 100
+
+var sampleStrings []string
+
+func init() {
+	for i := 0; i < numStrings; i++ {
+		sampleStrings = append(sampleStrings, fmt.Sprintf("String %d", i))
+	}
+}
+
+func concatenateUsingJoin(parts []string) string {
+	return strings.Join(parts, " ")
+}
+
+func concatenateUsingPlus(parts []string) string {
+	result := ""
+	for _, part := range parts {
+		result += part + " "
+	}
+	return result[:len(result)-1]
+}
+
+func BenchmarkConcatenateUsingJoin(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		concatenateUsingJoin(sampleStrings)
+	}
+}
+
+func BenchmarkConcatenateUsingPlus(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		concatenateUsingPlus(sampleStrings)
+	}
+}
+
+func main() {
+	testing.Main()
+}
--- RLHF/golang-random_7/1-1-25/493852/turn3/modelB/main.go
+++ RLHF/golang-random_7/1-1-25/493852/turn3/modelB/main.go
@@ -1,52 +1,42 @@
 package main
 
 import (
-	"fmt"
-	"regexp"
+	"testing"
 )
 
-// User represents a user with sensitive information.
-type User struct {
-	Username string `json:"username"`
-	Age      int    `json:"age"`
-}
+const numStrings = 10000
+const stringLength = 100
 
-// SanitizedUsers is a slice of sanitized User.
-type SanitizedUsers []string
-
-// sanitizeUsername removes any special characters from the username.
-func sanitizeUsername(username string) string {
-	re := regexp.MustCompile(`[^a-zA-Z0-9]`)
-	return re.ReplaceAllString(username, "")
-}
-
-// sanitizeAge formats the age to display only the last two digits.
-func sanitizeAge(age int) string {
-	return fmt.Sprintf("%02d", age%100)
-}
-
-// FormatUsers safely formats a list of users with sanitized information.
-func FormatUsers(users []User) SanitizedUsers {
-	sanitizedUsers := make(SanitizedUsers, len(users))
-	for i, user := range users {
-		sanitizedUsername := sanitizeUsername(user.Username)
-		sanitizedAge := sanitizeAge(user.Age)
-		sanitizedUsers[i] = fmt.Sprintf("Username: %s, Age: %s", sanitizedUsername, sanitizedAge)
+func generateStrings() []string {
+	strings := make([]string, numStrings)
+	for i := 0; i < numStrings; i++ {
+		strings[i] = strings.Repeat("a", stringLength)
 	}
-	return sanitizedUsers
+	return strings
 }
 
-func main() {
-	users := []User{
-		{Username: "alice!@#$", Age: 25},
-		{Username: "bob123", Age: 30},
-		{Username: "charles_321", Age: -10},
-		{Username: "daisy-54", Age: 100},
+func concatenateStrings(strings []string) string {
+	var result string
+	for _, str := range strings {
+		result += str
 	}
+	return result
+}
 
-	sanitizedUsers := FormatUsers(users)
+func joinStrings(strings []string) string {
+	return strings.Join(strings, "")
+}
 
-	for _, sanitizedUser := range sanitizedUsers {
-		fmt.Println(sanitizedUser)
+func BenchmarkConcatenation(b *testing.B) {
+	strings := generateStrings()
+	for i := 0; i < b.N; i++ {
+		concatenateStrings(strings)
+	}
+}
+
+func BenchmarkJoin(b *testing.B) {
+	strings := generateStrings()
+	for i := 0; i < b.N; i++ {
+		joinStrings(strings)
 	}
 }
