a235125baaf9e709a53238dacf318bc27396bf79
pankaj-solanki1 <katyarepankaj31@gmail.com>
2025-01-12 23:14:28 +0530

    512390

--- RLHF/golang-random_7/12-1-25/512390/turn3/modelA/main.go
+++ RLHF/golang-random_7/12-1-25/512390/turn3/modelA/main.go
@@ -2,23 +2,73 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
-func generateFormattedIDs(format string, start int, end int, batchSize int) {
-	for i := start; i <= end; i += batchSize {
-		batch := make([]string, 0)
-		for j := 0; j < batchSize && i+j <= end; j++ {
-			batch = append(batch, fmt.Sprintf(format, i+j))
-		}
-		fmt.Println("Batch", (i/batchSize)+1, ":", batch)
+type State string
+
+const (
+	InitState     State = "INIT"
+	Step1State    State = "STEP_1"
+	Step2State    State = "STEP_2"
+	Step3State    State = "STEP_3"
+	CompleteState State = "COMPLETE"
+)
+
+type MultiStepProcess struct {
+	currentState State
+	mutex        sync.Mutex
+}
+
+func NewMultiStepProcess() *MultiStepProcess {
+	return &MultiStepProcess{currentState: InitState}
+}
+
+func (m *MultiStepProcess) run() {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	switch m.currentState {
+	case InitState:
+		fmt.Println("Starting process...")
+		m.transition(Step1State)
+	case Step1State:
+		fmt.Println("Performing step 1...")
+		m.simulateTask(Step1State)
+		m.transition(Step2State)
+	case Step2State:
+		fmt.Println("Performing step 2...")
+		m.simulateTask(Step2State)
+		m.transition(Step3State)
+	case Step3State:
+		fmt.Println("Performing step 3...")
+		m.simulateTask(Step3State)
+		m.transition(CompleteState)
+	case CompleteState:
+		fmt.Println("Process completed.")
+	default:
+		fmt.Println("Invalid state:", m.currentState)
 	}
 }
 
-func main() {
-	format := "ID%05d"
-	start := 1
-	end := 100
-	batchSize := 10
+func (m *MultiStepProcess) transition(nextState State) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
 
-	generateFormattedIDs(format, start, end, batchSize)
+	m.currentState = nextState
+}
+
+func (m *MultiStepProcess) simulateTask(state State) {
+	fmt.Println("Simulating task for", state, "for 2 seconds...")
+	time.Sleep(2 * time.Second)
+	fmt.Println("Task for", state, "completed.")
+}
+
+func main() {
+	process := NewMultiStepProcess()
+	go process.run()
+
+	// Optionally, you can add a main loop to keep the program running
+	select {}
 }
--- RLHF/golang-random_7/12-1-25/512390/turn3/modelB/main.go
+++ RLHF/golang-random_7/12-1-25/512390/turn3/modelB/main.go
@@ -2,38 +2,85 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
 
-func generateIDStrings(start int, end int, batchSize int) [][]string {
-	var results [][]string
+type State int
 
-	for i := start; i <= end; i += batchSize {
-		endBatch := i + batchSize - 1
-		if endBatch > end {
-			endBatch = end
-		}
-		var batch []string
-		for j := i; j <= endBatch; j++ {
-			str := fmt.Sprintf("ID%06d", j)
-			batch = append(batch, str)
-		}
-		results = append(results, batch)
+const (
+	InitialState State = iota
+	Step1
+	Step2
+	Step3
+	FinalState
+)
+
+type StateMachine struct {
+	currentState State
+	wg           sync.WaitGroup
+}
+
+func (sm *StateMachine) Start() {
+	sm.wg.Add(1)
+	defer sm.wg.Done()
+	sm.transition(InitialState)
+}
+
+func (sm *StateMachine) transition(nextState State) {
+	sm.currentState = nextState
+	switch nextState {
+	case InitialState:
+		sm.handleInitialState()
+	case Step1:
+		sm.handleStep1()
+	case Step2:
+		sm.handleStep2()
+	case Step3:
+		sm.handleStep3()
+	case FinalState:
+		sm.handleFinalState()
 	}
-	return results
+}
+
+func (sm *StateMachine) handleInitialState() {
+	fmt.Println("Entering Initial State")
+	// Simulate a time-consuming task for Step 1
+	time.Sleep(2 * time.Second)
+	sm.transition(Step1)
+}
+
+func (sm *StateMachine) handleStep1() {
+	fmt.Println("Entering Step 1")
+	// Simulate a time-consuming task for Step 2
+	time.Sleep(3 * time.Second)
+	sm.transition(Step2)
+}
+
+func (sm *StateMachine) handleStep2() {
+	fmt.Println("Entering Step 2")
+	// Simulate a time-consuming task for Step 3
+	time.Sleep(1 * time.Second)
+	sm.transition(Step3)
+}
+
+func (sm *StateMachine) handleStep3() {
+	fmt.Println("Entering Step 3")
+	// Simulate a time-consuming task for Final State
+	time.Sleep(4 * time.Second)
+	sm.transition(FinalState)
+}
+
+func (sm *StateMachine) handleFinalState() {
+	fmt.Println("Entering Final State")
+	// Terminate the state machine
+	sm.wg.Done()
 }
 
 func main() {
-	start := 1
-	end := 100
-	batchSize := 10
-
-	batches := generateIDStrings(start, end, batchSize)
-
-	for i, batch := range batches {
-		fmt.Println("Batch", i+1, ":")
-		for _, str := range batch {
-			fmt.Print(str, " ")
-		}
-		fmt.Println()
-	}
+	sm := &StateMachine{}
+	go sm.Start()
+	fmt.Println("Waiting for the state machine to complete...")
+	sm.wg.Wait()
+	fmt.Println("State machine completed.")
 }
