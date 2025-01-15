c3c6aefda5c4a61e288f592c162629c2f523e399
pankaj-solanki1 <katyarepankaj31@gmail.com>
2025-01-15 23:36:26 +0530

    512479

--- RLHF/golang-random_7/15-1-25/512479/turn3/modelA/main.go
+++ RLHF/golang-random_7/15-1-25/512479/turn3/modelA/main.go
@@ -1,92 +1,20 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
-
-type Cell struct {
-	value   int
-	readers int
-	mu      sync.Mutex
-	wg      sync.WaitGroup
-}
-
-type Table struct {
-	cells map[int]*Cell
-}
-
-func (t *Table) CreateCell(key int) {
-	t.cells[key] = &Cell{}
-}
-
-func (c *Cell) Read() int {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.readers++
-	c.wg.Add(1)
-	defer c.wg.Done()
-	return c.value
-}
-
-func (c *Cell) Write(value int) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	// Wait for all readers to finish
-	c.wg.Wait()
-	c.value = value
-}
-
-func (t *Table) GetCell(key int) *Cell {
-	return t.cells[key]
-}
-
-func (t *Table) UpdateCell(key int, value int) {
-	cell := t.GetCell(key)
-	if cell != nil {
-		cell.Write(value)
-	}
-}
+import "fmt"
 
 func main() {
-	table := &Table{
-		cells: make(map[int]*Cell),
-	}
+	// Define the variables to be used in the formatted string
+	name := "Bob"
+	age := 30
+	profession := "Engineer"
 
-	// Create some cells concurrently
-	for i := 1; i <= 5; i++ {
-		go func(id int) {
-			table.CreateCell(id)
-		}(i)
-	}
+	// Format the string using fmt.Sprintf and store it in a variable
+	formattedString := fmt.Sprintf("%s is a %d-year-old %s.", name, age, profession)
 
-	// Reader goroutines
-	for i := 1; i <= 5; i++ {
-		go func(readerID int) {
-			for {
-				key := readerID%5 + 1 // Select a cell to read based on reader ID
-				cell := table.GetCell(key)
-				if cell != nil {
-					fmt.Printf("Reader %d reads cell %d: %d\n", readerID, key, cell.Read())
-				}
-				time.Sleep(time.Duration(readerID) * time.Millisecond)
-			}
-		}(i)
-	}
+	// Print the initial formatted string
+	fmt.Println(formattedString)
 
-	// Writer goroutines
-	for i := 1; i <= 5; i++ {
-		go func(writerID int) {
-			for {
-				key := writerID%5 + 1 // Select a cell to write based on writer ID
-				table.UpdateCell(key, key*writerID)
-				fmt.Printf("Writer %d updates cell %d: %d\n", writerID, key, key*writerID)
-				time.Sleep(time.Duration(writerID) * time.Millisecond)
-			}
-		}(i)
-	}
-
-	// You can optionally use `time.After()` to stop the application
-	time.Sleep(5 * time.Second)
+	// Append additional information to the formatted string and print it
+	modifiedString := formattedString + " He has been working at TechCorp for 5 years."
+	fmt.Println(modifiedString)
 }
--- RLHF/golang-random_7/15-1-25/512479/turn3/modelB/main.go
+++ RLHF/golang-random_7/15-1-25/512479/turn3/modelB/main.go
@@ -1,75 +1,20 @@
 package main
 
 import (
-	"sync"
+	"fmt"
 )
 
-type Cell struct {
-	value     int
-	readCount int
-	mu        sync.Mutex
-	wg        sync.WaitGroup
-}
-
-type Table struct {
-	cells map[int]*Cell
-	mu    sync.Mutex
-}
-
-func (t *Table) CreateCell(key int) {
-	t.mu.Lock()
-	defer t.mu.Unlock()
-	if _, ok := t.cells[key]; !ok {
-		t.cells[key] = &Cell{}
-	}
-}
-
-func (t *Table) GetCell(key int) *Cell {
-	t.mu.Lock()
-	defer t.mu.Unlock()
-	return t.cells[key]
-}
-
-func (c *Cell) Read() int {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	c.readCount++
-	defer func() {
-		c.readCount--
-		if c.readCount == 0 {
-			c.wg.Broadcast() // Wake up all writers waiting for this cell
-		}
-	}()
-	return c.value
-}
-
-func (c *Cell) Write(value int) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	for c.readCount > 0 {
-		c.wg.Wait() // Wait for all readers to complete
-	}
-	c.value = value
-}
-
-func (t *Table) UpdateCell(key int, value int) {
-	cell := t.GetCell(key)
-	if cell != nil {
-		cell.Write(value)
-	}
-}
-
-func (t *Table) ReadCell(key int) int {
-	cell := t.GetCell(key)
-	if cell != nil {
-		return cell.Read()
-	}
-	return 0
-}
-
 func main() {
-	table := &Table{
-		cells: make(map[int]*Cell),
-	}
-	// Rest of the code remains the same...
+	name := "James"
+	planet := "Mars"
+	time := "22:15"
+
+	// Using fmt.Sprintf to format a string with multiple variables and store it in a variable
+	formattedString := fmt.Sprintf("Greetings, %s! It's currently %s on %s.", name, time, planet)
+
+	// Modifying the formatted string by appending additional information
+	modifiedString := fmt.Sprintf("%s I hope you are doing well. I just completed a mission to %s.", formattedString, planet)
+
+	// Printing the modified string using fmt.Println
+	fmt.Println(modifiedString)
 }
