b2c92c974e9ce5a95a6c5bd651caa648612caf7a
pankaj-solanki1 <katyarepankaj31@gmail.com>
2025-01-19 16:09:23 +0530

    512351

--- RLHF/golang-random_7/19-1-25/512351/turn3/modelA/main.go
+++ RLHF/golang-random_7/19-1-25/512351/turn3/modelA/main.go
@@ -1,147 +1,32 @@
 package main
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-	"encoding/base64"
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"sync"
+	"os"
+	"path/filepath"
+	"time"
 )
-
-// Define privacy levels
-type PrivacyLevel int
-
-const (
-	Public PrivacyLevel = iota
-	Private
-	Sensitive
-)
-
-const encryptionKey = "examplekey123456" // Encryption key (16 bytes for AES-128)
-
-var paramPrivacyLevels = map[string]PrivacyLevel{
-	"username": Public,
-	"email":    Private,
-	"ssn":      Sensitive,
-}
-
-// Encrypt uses AES-GCM to encrypt sensitive data
-func encrypt(data string) (string, error) {
-	block, err := aes.NewCipher([]byte(encryptionKey))
-	if err != nil {
-		return "", err
-	}
-
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return "", err
-	}
-
-	nonce := make([]byte, gcm.NonceSize())
-	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		return "", err
-	}
-
-	encryptedData := gcm.Seal(nonce, nonce, []byte(data), nil)
-	return base64.StdEncoding.EncodeToString(encryptedData), nil
-}
-
-// Decrypt uses AES-GCM to decrypt sensitive data
-func decrypt(encryptedData string) (string, error) {
-	decodedData, err := base64.StdEncoding.DecodeString(encryptedData)
-	if err != nil {
-		return "", err
-	}
-
-	block, err := aes.NewCipher([]byte(encryptionKey))
-	if err != nil {
-		return "", err
-	}
-
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return "", err
-	}
-
-	nonceSize := gcm.NonceSize()
-	if len(decodedData) < nonceSize {
-		return "", fmt.Errorf("ciphertext too short")
-	}
-
-	nonce, ciphertext := decodedData[:nonceSize], decodedData[nonceSize:]
-	decryptedData, err := gcm.Open(nil, nonce, ciphertext, nil)
-	if err != nil {
-		return "", err
-	}
-
-	return string(decryptedData), nil
-}
-
-// Handler to process query parameters securely
-func queryParamHandler(w http.ResponseWriter, r *http.Request) {
-	queryParams := r.URL.Query()
-	var wg sync.WaitGroup
-
-	results := make(chan string, len(queryParams))
-
-	for key, value := range queryParams {
-		// Capture the key and value for the goroutine
-		key, value := key, value
-
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-
-			if len(value) == 0 {
-				results <- fmt.Sprintf("%s: No value provided\n", key)
-				return
-			}
-
-			var processedValue string
-			var err error
-
-			switch paramPrivacyLevels[key] {
-			case Public:
-				processedValue = value[0] // Direct usage
-				results <- fmt.Sprintf("Public %s: %s\n", key, processedValue)
-
-			case Private:
-				// Assume hashing or pseudonymization is done here
-				processedValue = "hashed_" + value[0] // Simplified for demonstration
-				results <- fmt.Sprintf("Private %s (Hashed): %s\n", key, processedValue)
-
-			case Sensitive:
-				// Encrypt the value
-				processedValue, err = encrypt(value[0])
-				if err != nil {
-					log.Printf("Encryption error: %v", err)
-					results <- fmt.Sprintf("Sensitive %s: Error encrypting data\n", key)
-					return
-				}
-				results <- fmt.Sprintf("Sensitive %s (Encrypted): %s\n", key, processedValue)
-			}
-		}()
-	}
-
-	go func() {
-		wg.Wait()
-		close(results)
-	}()
-
-	for result := range results {
-		fmt.Fprint(w, result)
-	}
-}
 
 func main() {
-	http.HandleFunc("/query", queryParamHandler)
+	// Set the target directory and max age for retention
+	directory := "./data"         // Specify your directory path here
+	maxAge := 30 * 24 * time.Hour // Files older than 30 days will be deleted
 
-	fmt.Println("Server is listening on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+	// Get the current time
+	now := time.Now()
+
+	// Walk through the directory
+	filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			// Return early if there's an error accessing a file
+			return err
+		}
+
+		if !info.IsDir() {
+			// Check if the file is older than the specified duration
+			if now.Sub(info.ModTime()) > maxAge {
+				os.Remove(path) // Delete the file
+			}
+		}
+		return nil // Continue walking
+	})
 }
