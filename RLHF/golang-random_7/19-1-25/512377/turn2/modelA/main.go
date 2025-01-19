d213086dbe72895713297e0e038172ad13e822e8
pankaj-solanki1 <katyarepankaj31@gmail.com>
2025-01-19 15:35:41 +0530

    512377

--- RLHF/golang-random_7/19-1-25/512377/turn2/modelA/main.go
+++ RLHF/golang-random_7/19-1-25/512377/turn2/modelA/main.go
@@ -1,98 +1,50 @@
 package main
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/hmac"
-	"crypto/rand"
-	"crypto/sha256"
-	"encoding/base64"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"runtime/pprof"
+	"time"
 )
 
-// Defined privacy levels
-type PrivacyLevel int
+func processLargeData() {
+	data := make(map[string]int)
 
-const (
-	Public PrivacyLevel = iota
-	Private
-	Sensitive
-)
-
-const encryptionKey = "examplekey123456" // AES key for encryption (16 bytes)
-
-var paramPrivacyLevels = map[string]PrivacyLevel{
-	"username": Public,
-	"email":    Private,
-	"ssn":      Sensitive,
-}
-
-// Encrypt sensitive data
-func encrypt(data string) (string, error) {
-	block, err := aes.NewCipher([]byte(encryptionKey))
-	if err != nil {
-		return "", err
+	for i := 0; i < 1000000; i++ {
+		key := fmt.Sprintf("key-%d", i%1000)
+		data[key]++
 	}
 
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return "", err
+	total := 0
+	for _, v := range data {
+		total += v
 	}
 
-	nonce := make([]byte, gcm.NonceSize())
-	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		return "", err
-	}
+	fmt.Println("Total:", total)
 
-	encryptedData := gcm.Seal(nonce, nonce, []byte(data), nil)
-	return base64.StdEncoding.EncodeToString(encryptedData), nil
-}
-
-// Handle query parameters with respect to their privacy level
-func queryParamHandler(w http.ResponseWriter, r *http.Request) {
-	queryParams := r.URL.Query()
-
-	for key, value := range queryParams {
-		if len(value) == 0 {
-			continue
-		}
-
-		var processedValue string
-
-		switch paramPrivacyLevels[key] {
-		case Public:
-			processedValue = value[0] // Direct usage
-			fmt.Fprintf(w, "Public %s: %s\n", key, processedValue)
-
-		case Private:
-			// Hash the value using HMAC-SHA256
-			h := hmac.New(sha256.New, []byte(encryptionKey))
-			h.Write([]byte(value[0]))
-			processedValue = base64.StdEncoding.EncodeToString(h.Sum(nil))
-			fmt.Fprintf(w, "Private %s (Hashed): %s\n", key, processedValue)
-
-		case Sensitive:
-			// Encrypt the value
-			encryptedValue, err := encrypt(value[0])
-			if err != nil {
-				log.Printf("Encryption error: %v", err)
-				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-				return
-			}
-			processedValue = encryptedValue
-			fmt.Fprintf(w, "Sensitive %s (Encrypted): %s\n", key, processedValue)
-		}
-	}
 }
 
 func main() {
-	http.HandleFunc("/query", queryParamHandler)
+	// Start the pprof server
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
 
-	fmt.Println("Server is listening on :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	// Optionally create a CPU profile file
+	f, err := os.Create("cpu.prof")
+	if err != nil {
 		log.Fatal(err)
 	}
+
+	defer f.Close()
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
+
+	// Run the data processing function
+	processLargeData()
+	time.Sleep(60 * time.Second)
+
 }
