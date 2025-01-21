5eb6d0052f1ba090ceab71dc7ffa2afee4dc1545
pankaj_solanki1 <katyarepankaj31@gmail.com>
2025-01-21 16:39:59 +0530

    528279

---
+++ RLHF/golang_random_9/21-1-25/528279/turn3/modelB/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func processData(data []int, wg *sync.WaitGroup, id int) {
+	defer wg.Done()
+	for _, value := range data {
+		fmt.Printf("Goroutine %d processing: %d\n", id, value)
+	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	dataSlices := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+		{10, 11, 12},
+	}
+
+	for id, data := range dataSlices {
+		wg.Add(1)
+		go processData(data, &wg, id+1) // Add +1 to make id start from 1
+	}
+
+	wg.Wait()
+	fmt.Println("All data processed.")
+}
